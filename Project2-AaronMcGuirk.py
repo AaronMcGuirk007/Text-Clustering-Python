eacac4f46fa99a5363e78176419d10e99926c3ef
AaronMcGuirk007 <am12mcgu@siena.edu>
2022-04-25 14:10:57 -0400

    added files

---
+++ Project2-AaronMcGuirk.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 12:24:15 2022
+
+@author: Aaron-PC
+"""
+
+"""
+Project2-AaronMcGuirk.py
+
+Created on Sun Feb 20 22:26:15 2022
+
+Text clustering HW#2 for CSIS320
+
+@author: Aaron McGuirk
+@version: Spring 2022
+"""
+
+data = np.loadtxt("descriptions.txt", dtype="str", delimiter="\t", skiprows=1)
