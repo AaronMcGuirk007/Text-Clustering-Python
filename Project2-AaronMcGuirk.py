284d8111cb520147468b1772dbb8525054e1e3a2
AaronMcGuirk007 <am12mcgu@siena.edu>
2022-04-27 17:29:29 -0400

    added

--- Project2-AaronMcGuirk.py
+++ Project2-AaronMcGuirk.py
@@ -30,83 +30,115 @@ Notes:
     School of Business = 3
 """
 
+num_schools = 3
+schools = ["School of Liberal Arts", "School of Science", "School of Business"]
+
+num_departments = 33
+
+num_prefixes = 57
+
+num_optimize = 0
+
 data = np.loadtxt("descriptions.txt", dtype="str", delimiter="\t", skiprows=1)
-y_km = data
-TBD = 1
+data_vec = TfidVectorizer("descriptions.txt")
+#y_km = data
+#TBD = 1
 
 
-classes_vec = CountVectorizer().fit(data)
-print(classes_vec.vocabulary_)
-
-def clustering_3_groups():
+def numOfSchoolsClustering(data, clusters):
     
-    km = KMeans(n_clusters=3)
+    km = kMeans(n_clusters=clusters, solver='kmeans', max_df=100, learning_rate = 1)
     km.fit(data)
-    y_km = km.predict(data)
+    centers = km.cluster_centers.argssort()
+    school_labels = vectorizer.get_feature_names()
     
-    plt.scatter(data[:, 0], data[:, 1], c=y_km, s=50, cmap='viridis')
-    centers = km.cluster_centers_
-    plt.scatter(centers[:, 0], centers[:, 1], c='red', s=1314, alpha=0.5)
+    #y_km = km.predict(data)
+   
+    #plt.scatter(data[:, 0], data[:, 1], c=y_km, s=50, cmap='viridis')
+    #centers = km.cluster_centers_
+    #plt.scatter(centers[:, 0], centers[:, 1], c='red', s=1314, alpha=0.5)
+   
+    #ag = AgglomerativeClustering(n_clusters = 3).fit(data)
+    #centers = avg_clusters(data,ag.labels_)
     
-    ag = AgglomerativeClustering(n_clusters = 3).fit(data)
-    centers = avg_clusters(data,ag.labels_)
+    print("Number of Schools Cluster: ")
     
-    
-    for i in range(len(data)):
-        print(data[i])
+    for i in range(len(schools)):
+        print(schools[i] + ": ")
+        for j in centers[i, :num_schools]:
+            print(centers)[j]
         
         
-def clustering_33_groups():
+def numOfDepartmentsClustering(data, clusters):
     
-    km2 = kMeans(n_clusters=33)
-    km2.fit(data)
-    y_km2 = km2.predict(data)
+    km = kMeans(n_clusters=clusters, solver='kmeans', max_df=100, learning_rate = 1)
+    km.fit(data)
+    centers = km.cluster_centers.argssort()
+    school_labels = vectorizer.get_feature_names()
     
-    plt.scatter(data[:, 0], data[:, 1], c=y_km2, s=50, cmap='viridis')
-    centers2 = km2.cluster_centers_
-    plt.scatter(centers2[:, 0], centers2[:, 1], c='red', s=1314, alpha=0.5)
+    #y_km2 = km2.predict(data)
     
-    ag2 = AgglomerativeClustering(n_clusters = 33).fit(data)
-    centers2 = avg_clusters(data,ag2.labels_)
+    #plt.scatter(data[:, 0], data[:, 1], c=y_km2, s=50, cmap='viridis')
+    #centers2 = km2.cluster_centers_
+    #plt.scatter(centers2[:, 0], centers2[:, 1], c='red', s=1314, alpha=0.5)
+   
+    #ag2 = AgglomerativeClustering(n_clusters = 33).fit(data)
+    #centers2 = avg_clusters(data,ag2.labels_)
     
-    for i in range(len(data)):
-        print(data[i])
+    print("Number of Departments Cluster: ")
+    
+    for i in range(num_department):
+        print("Dept. Cluster")
+        for j in centers[i, :num_departments]:
+            print(centers[j])
 
 
-def clustering_57_groups():
+def numOfPrefixsClustering(data, clusters):
     
-    km3 = kMeans(n_clusters=57)
-    km3.fit(data)
-    y_km3= km3.predict(data)
+    km = kMeans(n_clusters=clusters, solver='kmeans', max_df=100, learning_rate = 1)
+    km.fit(data)
+    centers = km.cluster_centers.argssort()
+    school_labels = vectorizer.get_feature_names()
     
-    plt.scatter(data[:, 0], data[:, 1], c=y_km3, s=50, cmap='viridis')
-    center3 = km3.cluster_centers_
-    plt.scatter(centers3[:, 0], centers3[:, 1], c='red', s=1314, alpha=0.5)
+    #y_km3= km3.predict(data)
+   
+    #plt.scatter(data[:, 0], data[:, 1], c=y_km3, s=50, cmap='viridis')
+    #center3 = km3.cluster_centers_
+    #plt.scatter(centers3[:, 0], centers3[:, 1], c='red', s=1314, alpha=0.5)
+   
+    #ag3 = AgglomerativeClustering(n_clusters = 57).fit(data)
+    #centers3 = avg_clusters(data,ag3.labels_)
     
-    ag3 = AgglomerativeClustering(n_clusters = 57).fit(data)
-    centers3 = avg_clusters(data,ag3.labels_)
+    print("Number of Prefixes Cluster: ")
     
-    for i in range(len(data)):
-        print(data[i])
+    for i in range(num_prefixes):
+        print("Prefix Cluster")
+        for j in centers[i, :num_prefixes]:
+            print(centers[j])
     
     
+def numOptimalClustering(data, clusters):
     
-def clustering_optimal_groups():
+    km = kMeans(n_clusters=clusters, solver='kmeans', max_df=100, learning_rate = 1)
+    km.fit(data)
+    centers = km.cluster_centers.argssort()
+    school_labels = vectorizer.get_feature_names()
     
-    kmOpt = kMeans(n_clusters=TBD)
-    kmOpt.fit(data)
-    y_kmOpt = kmOpt.predict(data)
+    #y_kmOpt = kmOpt.predict(data)
     
-    plt.scatter(data[:, 0], data[:, 1], c=y_kmOpt, s=50, cmap='viridis')
-    centers4 = kmOpt.cluster_centers_
-    plt.scatter(centers4[:, 0], centers4[:, 1], c='red', s=1314, alpha=0.5)
+    #plt.scatter(data[:, 0], data[:, 1], c=y_kmOpt, s=50, cmap='viridis')
+    #centers4 = kmOpt.cluster_centers_
+    #plt.scatter(centers4[:, 0], centers4[:, 1], c='red', s=1314, alpha=0.5)
     
-    ag4 = AgglomerativeClustering(n_clusters = TBD).fit(data)
-    centers4 = avg_clusters(data,ag4.labels_)
+    #ag4 = AgglomerativeClustering(n_clusters = TBD).fit(data)
+    #centers4 = avg_clusters(data,ag4.labels_)
     
-    for i in range(len(data)):
-        print(data[i])
+    print("Number of Prefixes Cluster: ")
     
+    for i in range(num_optimize):
+        print("Prefix Cluster")
+        for j in centers[i, :num_optimize]:
+            print(centers[j])
 
         
 def avg_clusters(data,clusters):
@@ -121,3 +153,13 @@ def avg_clusters(data,clusters):
     return result
         
         
+def test_methods():
+    
+    # Tests all of the clustering methods!
+    print(numOfSchoolsClustering(data, num_schools))
+    
+    print(numOfDepartmentsClustering(data, num_departments))
+    
+    print(numOfPrefixsClustering(data, num_prefixes))
+    
+    print(numOptimalClustering(data, num_optimize))
